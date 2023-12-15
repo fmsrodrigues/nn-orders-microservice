a6c9e2961259d3b2d1700c423c332ca2bbd25db8
Phobia <filipe_msr@hotmail.com>
2023-12-15 17:23:14 -0300

    Refactor project structure

--- main.go
+++ main.go
@@ -1,30 +1,16 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"context"
+	"fmt"
+	"orders/application"
 )
 
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
+	app := application.NewApp()
 
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 	if err != nil {
-		log.Fatalf("Could not start server: %s\n", err.Error())
+		fmt.Println("Failed to start server: ", err)
 	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
 }
